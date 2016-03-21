6f7c1c18697b2539418293316d2d60335c3d3b35
Anthony Giallella <ajg9000@rit,edu>
2016-03-21 17:31:49 -0400

    1st commit

---
+++ client/maker.js
@@ -0,0 +1,55 @@
+"use strict";
+
+$(document).ready(function() {
+
+    function handleError(message) {
+        $("#errorMessage").text(message);
+        $("#domoMessage").animate({width:'toggle'},350);
+    }
+    
+    function sendAjax(action, data) {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            url: action,
+            data: data,
+            dataType: "json",
+            success: function(result, status, xhr) {
+                $("#domoMessage").animate({width:'hide'},350);
+
+                window.location = result.redirect;
+            },
+            error: function(xhr, status, error) {
+                var messageObj = JSON.parse(xhr.responseText);
+            
+                handleError(messageObj.error);
+            }
+        });        
+    }
+    
+    $("#makeDomoSubmit").on("click", function(e) {
+        e.preventDefault();
+        $("#domoMessage").animate({width:'hide'},350);
+    
+        if($("#domoName").val() == '' || $("#domoAge").val() == ''|| $("#domoLevel").val() == '') {
+            handleError("RAWR! All fields are required");
+            return false;
+        }
+
+        sendAjax($("#domoForm").attr("action"), $("#domoForm").serialize());
+        
+        return false;
+    });
+	 $("#doDomoDelete").on("click", function(e) {
+		e.preventDefault();
+		$("#domoMessage").animate({width:'hide'},350);
+    
+        if($("#domoDelName").val() == '' || $("#domoDelAge").val() == ''|| $("#domoDelLevel").val() == '') {
+            handleError("RAWR! All fields are required");
+            return false;
+        }
+        sendAjax($("#domoDelForm").attr("action"), $("#domoDelForm").serialize());
+        
+        return false;
+    });
+});
---
+++ src/controllers/Domo.js
@@ -0,0 +1,71 @@
+var _ = require('underscore');
+var models = require('../models');
+
+var Domo = models.Domo;
+var delName;
+var delAge;
+var delLevel
+var makerPage = function(req,res){
+	Domo.DomoModel.findByOwner(req.session.account._id, function(err, docs){
+		if(err){
+			console,log(err);
+			return res.status(400).json({error: 'An error occured'});
+		}
+		res.render('app',{csrfToken: req.csrfToken(), domos:docs});
+	});
+};
+var makeDomo = function(req,res){
+	if(!req.body.name || !req.body.age){
+		return res.status(400).json({error: "RAWR! name age and level are required"});
+	}
+	var domoData = {
+		name: req.body.name,
+		age: req.body.age,
+		level: req.body.level,
+		owner: req.session.account._id
+	};
+	
+	var newDomo = new Domo.DomoModel(domoData);
+	
+	newDomo.save(function(err){
+		if(err){
+			console.log(err);
+			return res.status(400).json({error: "An error occurred"});
+		}
+		res.json({redirect: '/maker'});
+	});
+};
+var deleteDomo = function(req,res){
+	var domoData = {
+		name: req.body.name,
+		age: req.body.age,
+		level: req.body.level,
+		owner: req.session.account._id
+	};
+	
+    Domo.findByName(domoData.name, function(err, doc) {
+        //errs, handle them
+        if(err) {
+            return res.json({err:err}); //if error, return it            
+        }
+        
+        //if no matches, let them know (does not necessarily have to be an error since technically it worked correctly)
+        if(!doc) {
+            return res.json({error: "No Dogs found"});
+        }
+		
+		doc.remove(function(err) {
+			if(err) {
+				return res.json({err:err}); //if error, return it
+			}
+        
+        //return success
+			res.json({redirect: '/maker'});
+		});
+        //Save the newCat object to the database
+        //set the lastAdded cat to our newest cat object. This way we can update it dynamically
+    });
+};
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
+module.exports.del = deleteDomo;
